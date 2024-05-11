c70e559772a068f83761b9186fca313e9ee3a130
QUANG <quangnguyenkz123@gmail.com>
2024-05-11 16:01:53 +0700

    Initial commit

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,30 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+
+import {PagesComponent} from './pages.component';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import { userComponent } from './userinfo/user.component';
+
+const routes: Routes = [{
+  path: '',
+  component: PagesComponent,
+  children: [
+    {
+      path: 'dashboard',
+      component: DashboardComponent,
+      data: {breadcrumb: 'Dashboard'},
+    },
+    {
+      path: 'userinfo',
+      component: userComponent,
+      data: {breadcrumb: 'User'},
+    }
+  ],
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {
+}
